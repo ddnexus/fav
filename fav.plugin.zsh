8d011a17be9bc6a0e945f763a1c9943006cae162
Domizio Demichelis <dd.nexus@gmail.com>
2020-11-22 15:46:53 +0700

    added env command

--- fav.plugin.zsh
+++ fav.plugin.zsh
@@ -2,6 +2,7 @@
 
 # Copyright (c) 2020 Domizio Demichelis - dd.nexus@gmail.com - https://github/com/ddnexus
 
+_fav_version="0.2.0"
 _fav_root=$(readlink -f $0 | xargs dirname)
 _fav_functions=$(ls -p "$_fav_root/functions" | grep -v /)
 fpath+=($_fav_root/functions)
