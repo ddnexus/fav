7f9cceafb9e71ade6dfa21065011a838cdadb4d6
Domizio Demichelis <dd.nexus@gmail.com>
2020-11-25 08:18:14 +0700

    removed _fav-init

--- fav.plugin.zsh
+++ fav.plugin.zsh
@@ -4,15 +4,21 @@
 
 _fav_version="0.2.0"
 _fav_root=$(readlink -f $0 | xargs dirname)
-_fav_functions=$(ls -p "$_fav_root/functions" | grep -v /)
 fpath+=($_fav_root/functions)
+autoload -Uz $(ls -p "$_fav_root/functions" | grep -v /)
 hash -df
 
 [[ -f ${FAV_FILE:=$HOME/.fav} ]] && source "$FAV_FILE" || { mkdir -p "$(dirname "$FAV_FILE")" && touch "$FAV_FILE" }
+FAV_DIR_PREVIEW_CMD=${FAV_DIR_PREVIEW_CMD:-$( _fav-available-cmd 'exa -lbg --git --color=always' 'ls -l' ) }
+FAV_FILE_PREVIEW_CMD=${FAV_FILE_PREVIEW_CMD:-$( _fav-available-cmd 'bat --paging=always' less more ) }
+export FAV_DIR_PREVIEW_CMD FAV_FILE_PREVIEW_CMD
+FAV_ENABLE_ICONS=${FAV_ENABLE_ICONS:-false}
+FAV_DIR_ICON=${FAV_DIR_ICON:-$($FAV_ENABLE_ICONS && echo ' ' || echo 'D')}         #     
+FAV_FILE_ICON=${FAV_FILE_ICON:-$($FAV_ENABLE_ICONS && echo ' ' || echo 'F')}       #     
+FAV_UNKNOWN_ICON=${FAV_UNKNOWN_ICON:-$($FAV_ENABLE_ICONS && echo '' || echo '?')}
 
 fav() {
 	local fav_cmd="$1"
-	autoload -Uz ${=_fav_functions} && _fav-init
 	if ! type "_fav-cli-$fav_cmd" > /dev/null; then
 		echo "[ERROR] Unknown fav command '$fav_cmd'!"
 		echo
@@ -24,7 +30,6 @@ fav() {
 }
 
 fav-widget() {
-	autoload -Uz ${=_fav_functions} && _fav-init
 	local accept=$(should-accept-line)
 	LBUFFER="${LBUFFER}$( _fav-widget )"
 	local ret=$?
